9953a79b0f92494549a55dd2014726f01a7f7975
zeshankhalid22 <116242001+zeshankhalid22@users.noreply.github.com>
2024-05-11 13:00:05 +0500

    request $operation $num1 $num to server.

--- Unix Special Files/client_server_using_fifo/client.c
+++ Unix Special Files/client_server_using_fifo/client.c
@@ -4,46 +4,40 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
+#define BUFSIZE 100
 
 int main() {
     int client_to_server;
-    char *myfifo = "/tmp/client_to_server_fifo";
+    char *to_server = "/tmp/client_to_server_fifo";
 
     int server_to_client;
-    char *myfifo2 = "/tmp/server_to_client_fifo";
+    char *to_client = "/tmp/server_to_client_fifo";
 
-    char buf[BUFSIZ];
+    char op;
     int num1, num2;
 
     while (1) {
-        printf("Input message to server: ");
-        scanf("%s", buf);
-        printf("Enter 2 numbers > ");
-        scanf("%d%d", &num1, &num2);
-
-        // open FIFO(myfifo) file with write mode,
-        // open() returns a file descriptor
-        client_to_server = open(myfifo, O_WRONLY);
-        // write data into the file descriptor
-        write(client_to_server, buf, sizeof(buf));
-        write(client_to_server, &num1, sizeof(num1));
-        write(client_to_server, &num2, sizeof(num2));
-        perror("Write: "); //Very crude error check
-
-        if(strcmp("exit", buf)){
-            printf("Exiting from Client.");
+        printf("Enter $op $num1 $num2 > ");
+        scanf(" %c", &op);
+        if (op == 'q') {
             break;
         }
+        scanf("%d%d", &num1, &num2);
 
-        server_to_client = open(myfifo2, O_RDONLY);
-        int sum;
-        read(server_to_client, &sum, sizeof(sum));
-        perror("Read: "); // Very crude error check
+        // Pass Request to Server
+        client_to_server = open(to_server, O_WRONLY);
+        write(client_to_server, &op, sizeof(op));
+        write(client_to_server, &num1, sizeof(num1));
+        write(client_to_server, &num2, sizeof(num2));
 
-        printf("... sum received from the server: %d\n", sum);
+        // Read response from server
+        server_to_client = open(to_client, O_RDONLY);
+        int result;
+        read(server_to_client, &result, sizeof(result));
 
+        printf("Response from the server: %d\n", result);
     }
-    // closing file descriptors
+
     close(client_to_server);
     close(server_to_client);
 
